586bd4245183d804063eb1e84464c11990ec8692
Assaf Shlomi <shlomi.assaf@gmail.com>
2019-12-16 10:38:27 +0200

    SWCDDTRAIN-995 - changes to the commit file

--- logoutControl.java
+++ logoutControl.java
@@ -4,7 +4,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello my lovely World!!");
+		System.out.println("Hello my first lovely World!!");
 	
 		
 }
